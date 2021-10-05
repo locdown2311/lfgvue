98af84b11f2b02dca94dc62b4e12dfdd47a34f3f
locdown2311 <igor2311@gmail.com>
2021-10-04 23:07:36 -0300

    teste de visualização de criar atividades

---
+++ tests/Feature/CreateActivityTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateActivityTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_view_create_activity_page()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->get('/activity/create');
+
+        $response->assertStatus(200);
+    }
+}
